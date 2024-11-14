995686e7886b14c1b4b5c34df1f356917835bc1a
yangheng <yanghengdaren@outlook.com>
2024-11-14 18:51:27 +0800

    利用Redis进行爬虫限制

--- src/main/java/com/hy/mianshibagu/controller/QuestionController.java
+++ src/main/java/com/hy/mianshibagu/controller/QuestionController.java
@@ -2,6 +2,7 @@ package com.hy.mianshibagu.controller;
 
 
 import cn.dev33.satoken.annotation.SaCheckRole;
+import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.json.JSONUtil;
 import com.alibaba.csp.sentinel.Entry;
 import com.alibaba.csp.sentinel.EntryType;
@@ -19,6 +20,7 @@ import com.hy.mianshibagu.common.ResultUtils;
 import com.hy.mianshibagu.constant.UserConstant;
 import com.hy.mianshibagu.exception.BusinessException;
 import com.hy.mianshibagu.exception.ThrowUtils;
+import com.hy.mianshibagu.manager.CounterManager;
 import com.hy.mianshibagu.model.dto.question.*;
 import com.hy.mianshibagu.model.dto.questionbank.QuestionBankQueryRequest;
 import com.hy.mianshibagu.model.entity.Question;
@@ -34,6 +36,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 题目接口
@@ -156,12 +159,52 @@ public class QuestionController {
     @GetMapping("/get/vo")
     public BaseResponse<QuestionVO> getQuestionVOById(long id, HttpServletRequest request) {
         ThrowUtils.throwIf(id <= 0, ErrorCode.PARAMS_ERROR);
+        // 检测和处置爬虫
+        User loginUser = userService.getLoginUser(request);
+        crawlerDetect(loginUser.getId());
         // 查询数据库
         Question question = questionService.getById(id);
         ThrowUtils.throwIf(question == null, ErrorCode.NOT_FOUND_ERROR);
         // 获取封装类
         return ResultUtils.success(questionService.getQuestionVO(question, request));
     }
+
+    //为开发方便
+    @Resource
+    private CounterManager counterManager;
+
+    /**
+     * 检测爬虫
+     *
+     * @param loginUserId
+     */
+    private void crawlerDetect(long loginUserId) {
+        // 调用多少次时告警
+        final int WARN_COUNT = 10;
+        // 超过多少次封号
+        final int BAN_COUNT = 20;
+        // 拼接访问 key
+        String key = String.format("user:access:%s", loginUserId);
+        // 一分钟内访问次数，180 秒过期
+        long count = counterManager.incrAndGetCounter(key, 1, TimeUnit.MINUTES, 180);
+        // 是否封号
+        if (count > BAN_COUNT) {
+            // 踢下线
+            StpUtil.kickout(loginUserId);
+            // 封号
+            User updateUser = new User();
+            updateUser.setId(loginUserId);
+            updateUser.setUserRole("ban");
+            userService.updateById(updateUser);
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR, "访问太频繁，已被封号");
+        }
+        // 是否告警
+        if (count == WARN_COUNT) {
+            // 可以改为向管理员发送邮件通知
+            throw new BusinessException(110, "警告访问太频繁");
+        }
+    }
+
 
     /**
      * 分页获取题目列表（仅管理员可用）
